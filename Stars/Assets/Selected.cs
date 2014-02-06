dda206eaada7b0b5b0fe0e85a4942434d0aa61de
SamSeidenberg <seidensj@gmail.com>
2014-02-06 16:50:28 -0500

    Initial commit
    Things has been put. (Initial commit)

---
+++ Stars/Assets/Selected.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selected : MonoBehaviour {
+
+	public bool hitSelected = false;
+	public string me;
+	public AudioClip hoverSound;
+	public AudioClip selectSound;
+	private detectStar DetectStar;
+	public bool preparedForSelect = false;
+	public bool isSelected = false;
+	public Vector3 originalScale;
+	public Vector3 highlightScale;
+
+	// Use this for initialization
+	void Start () {
+		DetectStar = Camera.main.gameObject.GetComponent<detectStar>();
+		originalScale = transform.localScale;
+		highlightScale  = originalScale*2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		me = DetectStar.thing.name;
+		if(me==this.gameObject.name){
+			Color selectedColor = new Color(0, 255, 0) * Time.deltaTime;
+			renderer.material.color = selectedColor;
+//			transform.localScale = new Vector3(2F, 2F, 2F);
+			transform.localScale = highlightScale;
+
+			preparedForSelect = true;
+			Debug.Log(preparedForSelect);
+		} else {
+			Color otherColor = new Color(255, 255, 255) * Time.deltaTime;
+			renderer.material.color = otherColor;
+			transform.localScale = originalScale;
+		}
+
+		if(preparedForSelect == true && Input.GetKey(KeyCode.J)) {
+			audio.PlayOneShot(selectSound,1);
+			isSelected = true;
+		}
+	
+	}
+
+//		if(hitSelected == true) { 
+//			
+//		}
+	}
+
+
+
---
+++ Stars/Assets/detectStar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detectStar : MonoBehaviour {
+
+	public int rayDistance = 1000;
+	public GameObject thing = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		Debug.DrawRay(transform.position, fwd*rayDistance, Color.green);
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, fwd, out hit, rayDistance) && hit.transform.gameObject){
+			thing = hit.transform.gameObject;
+//			if(thing.tag == "star"){
+//				thing.GetComponent<Selected>().hitSelected = true;
+//			} 
+
+		} else {
+			thing = null;
+		}
+	}
+}
