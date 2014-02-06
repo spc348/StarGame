5ae8a227d446503700c442c2c862d7659a2c1bdd
SamSeidenberg <seidensj@gmail.com>
2014-02-06 17:08:31 -0500

    Changed stupid variable names
    Changed “me” in Selected to currentStar

--- Stars/Assets/Selected.cs
+++ Stars/Assets/Selected.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Selected : MonoBehaviour {
 
 	public bool hitSelected = false;
-	public string me;
+	public string currentStar;
 	public AudioClip hoverSound;
 	public AudioClip selectSound;
 	private detectStar DetectStar;
@@ -22,8 +22,10 @@ public class Selected : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		me = DetectStar.thing.name;
-		if(me==this.gameObject.name){
+
+		//Establish that the star the Ray is hitting is the star this script is on
+		currentStar = DetectStar.thing.name;
+		if(currentStar==this.gameObject.name){
 			Color selectedColor = new Color(0, 255, 0) * Time.deltaTime;
 			renderer.material.color = selectedColor;
 //			transform.localScale = new Vector3(2F, 2F, 2F);
